8c8a7388c6b9cf9ca0cc869c4d35fae19e31cd3c
Kauã Rabelo <dudusiilva_15@outlook.com>
2022-10-24 17:14:11 -0300

    Revert "v2.0"
    This reverts commit 5f1041d448c66c27af957d09e1010d754408798e.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { CadastroComponent } from './cadastro/cadastro/cadastro.component';
-import { LoginComponent } from './views/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
   {path: 'cadastro', component: CadastroComponent},
-  {path: 'login', component: LoginComponent},
 ];
 
 @NgModule({
