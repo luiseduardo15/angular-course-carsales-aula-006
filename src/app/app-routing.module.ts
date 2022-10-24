07e9c009464f94258ad3215c7ee72b83ab5b6550
Kauã Rabelo <dudusiilva_15@outlook.com>
2022-10-24 17:21:55 -0300

    v2.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { CadastroComponent } from './cadastro/cadastro/cadastro.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
 
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
   {path: 'cadastro', component: CadastroComponent},
 ];
 
